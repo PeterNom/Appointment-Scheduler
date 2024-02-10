ae41aa01559a7155aefa0d04cef6a549449b0585
Panagiotis Nomikos <102253139+PeterNom@users.noreply.github.com>
2024-02-10 20:03:09 +0200

    Added the database.

---
+++ Appointment Scheduler/Data/AppointmentSchedulerDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Appointment_Scheduler.Models;
+using Microsoft.EntityFrameworkCore;
+using System.IO.Pipelines;
+
+namespace Appointment_Scheduler.Data
+{
+    public class AppointmentSchedulerDbContext : DbContext
+    {
+        public AppointmentSchedulerDbContext(DbContextOptions<AppointmentSchedulerDbContext> options) : 
+            base(options) { }
+         public DbSet<Appointment> Appointments { get; set; }
+    }
+}
--- Appointment Scheduler/Program.cs
+++ Appointment Scheduler/Program.cs
@@ -1,6 +1,29 @@
+using Appointment_Scheduler.Data;
+using Appointment_Scheduler.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IAppointmentRepository, AppointmentRepository>();
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppointmentSchedulerDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration["ConnectionStrings:AppointmentSchedulerDbContextConnection"]);
+});
+
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.MapDefaultControllerRoute();
 
 app.Run();
